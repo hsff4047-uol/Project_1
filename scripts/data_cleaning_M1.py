80ffcfaeddd30b10170c8c73c1e2a33eb86a6909
Emmanuel Ong <rffg8213@leeds.ac.uk>
2025-02-27 16:57:14 +0000

    Created import_data(url)

--- scripts/data_cleaning_M1.py
+++ scripts/data_cleaning_M1.py
@@ -1,1 +1,29 @@
+import requests
+import os
+
+url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2023-01-01&endtime=2023-01-02"
+
+def import_data(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        text_data = response.text
+    else:
+      raise Exception("Downloading data error")
+
+    # Checks if the 'data' folder exists
+    if not os.path.exists("data"):
+      os.makedirs("data")
+
+    # This joins my 'data' folder with my dataset file "dataset_M1.txt"
+    file_path = os.path.join("data", "dataset_M1.txt")
+
+    # Saving the downloaded data
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(text_data)
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        data_lines = f.readlines()
+
+    return data_lines
 
